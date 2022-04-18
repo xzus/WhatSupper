644d5df8e9d5f5ef3625f27e08aed6816131a306
promasaha <77773142+promasaha@users.noreply.github.com>
2022-04-18 02:22:04 -0500

    Added bootstrap modules annd navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import HomePage from './pages/homePage';
+import calendarPage from './pages/calendarPage'
+
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <NavBar/>
+
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/calendarPage">
+            <calendarPage />
+          </Route>
+        </Routes>
+      </BrowserRouter> */}
+
     </div>
   );
 }
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {Navbar, NavDropdown, Container, Nav} from "react-bootstrap";
+// import SearchBar from './SearchBar';
+// import logo from "../assets/logo.png";
+
+const NavBar = (props) => {
+    return( 
+        <Navbar bg="light" expand="lg">
+        <Container>
+        {/* <Navbar.Brand href="#home">
+            <img
+                src={logo}
+                width="50"
+                height="50"
+                className="d-inline-block align-top"
+                alt=""
+            />
+        </Navbar.Brand> */}
+            <Navbar.Brand href="#home">WhatSupper?</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="calendar">Calendar</Nav.Link>
+                <Nav.Link href="voting">Voting</Nav.Link>
+                <Nav.Link href="pastrecipes">Past Recipes</Nav.Link>
+                <Nav.Link href="groceries">Groceries</Nav.Link>
+                <Nav.Link href="pantry">Pantry</Nav.Link>
+            </Nav>
+            {/* <SearchBar></SearchBar> */}
+
+            </Navbar.Collapse>
+
+
+        </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar;
