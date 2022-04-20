f2a0b80c4a50d56acc3e65440675bb2c5b832d3c
Kevin <kevinchenftw@gmail.com>
2022-04-19 20:06:21 -0500

    Implemented backend API functionality

--- backend/whatSupper/admin.py
+++ backend/whatSupper/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import RecipeVotes
+
+class RecipeVotesAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'votes')
+
+admin.site.register(RecipeVotes, RecipeVotesAdmin)
--- backend/whatSupper/models.py
+++ backend/whatSupper/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class RecipeVotes(models.Model):
+    recipe = models.CharField(max_length=120)
+    votes = models.IntegerField()
+
+    def __str__(self):
+        return str(self.recipe) + " " + str(self.votes)
