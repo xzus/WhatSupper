ca413cc7861949854fb45de088a809692e320105
mdee <mara.manskie@utexas.edu>
2022-04-20 02:05:14 -0500

    Work on home and voting pages, backend not fully connected yet

--- frontend/src/pages/homePage.js
+++ frontend/src/pages/homePage.js
@@ -4,7 +4,8 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 function homePage() {
-    const titles = ["Family Voting", "Recipe", "Groceries", "Pantry"];
+    const titles = ["Family Voting", "Recipe History", "Groceries", "Pantry"];
+    const pics = ["https://www.pngkit.com/png/detail/99-993265_bar-graph-clip-art.png", "https://www.myflorence.org/docs/_full_/district/food%20service/healthy-food-clipart-vector.jpg?id=712&thumbwidth=190&fullwidth=500", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGVE4GjZj3_tMtX91BceD4NFcchxT8SPWmuw&usqp=CAU", "https://media.istockphoto.com/vectors/storeroom-or-pantry-cellar-with-wooden-cupboard-vector-id831687812?k=20&m=831687812&s=612x612&w=0&h=RyySzHCqLB1tf-c4gfUteDxGriK6UZf_gQfFADpAMsI="]
     return (
         <Container>
   <Row>
@@ -19,11 +20,11 @@ function homePage() {
   {Array.from({ length: 4 }).map((_, idx) => (
     <Col>
       <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Img/>
         <Card.Body>
           <Card.Title>{titles[idx]}</Card.Title>
           <Card.Text>
-            TODO
+            <img src={pics[idx]} width="400" height="300"></img>
           </Card.Text>
         </Card.Body>
       </Card>
