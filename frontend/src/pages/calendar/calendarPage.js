55afc8368aefc2fe82f8b4ff55f121594a8d06e6
earlpotts <epotts0629@gmail.com>
2022-04-27 00:15:22 -0500

    added array of data and adjusted page to pull from data.

--- frontend/src/pages/calendar/calendarPage.js
+++ frontend/src/pages/calendar/calendarPage.js
@@ -2,15 +2,36 @@ import React from 'react';
 import RecipeBlock from './RecipeBlock';
 import {Container, Row, Col} from 'react-bootstrap';
 
+const recipeData = [
+  {
+    num: 1,
+    color: 'pink',
+    recipeText :'Pasta w/Meat Sauce',
+    days: 'Monday, Tuesday, Wednesday'
+  },
+  {
+    num: 2,
+    color: 'blue',
+    recipeText :'Enchiladas',
+    days: 'Thursday, Friday, Saturday'
+  },
+  {
+    num: 3,
+    color: 'green',
+    recipeText :'Grilled Cheese Sandwiches',
+    days: 'Sunday'
+  }
+]
+
 function calendarPage() {
     return (
         <Container>
             <Row className="justify-content-center">
-              <RecipeBlock
-                num={1}
-                color='pink'
-                recipeText='Pasta w/Meat Sauce'
-                days='Monday, Tuesday, Wednesday'/>
+              {recipeData.map((recipe) => {
+                return (
+                  <RecipeBlock {...recipe}/>
+                );
+              })}
             </Row>
         </Container>
     )
