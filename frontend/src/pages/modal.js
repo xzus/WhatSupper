4c20faa9b44f22a18374652583bd9fe074817435
tejnadas <dasaritejna@gmail.com>
2022-05-01 17:08:55 -0500

    Added further functionality to Modal Code

--- frontend/src/pages/modal.js
+++ frontend/src/pages/modal.js
@@ -1,25 +1,53 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import { Component } from 'react';
 
-const Modal = props => {
+class Modal extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: this.props.activeItem,
+    };
+  }
+
+  render (){
+    const {onSave} = this.props;
     return(
-      <div className = "modal">
-        <div className = "modal-content">
-          <div className = "modal-header">
-            <h1> Add your own Receipe! </h1>
-          </div>
-          <div className = "modal-body">
-            <h3> Enter the name of your Recipe! </h3>
-            <h3> Enter a Description of your Recipe! </h3>
-            <h3> Insert a picture of the Recipe!  </h3>
-          </div>
-          <div className = "modal-footer">
-            <button type = "button"> Submit Now! </button>
-          </div>
-          
+    <div className="modal">
+    <form onSubmit = {this.handleSubmit}>
+      <div className="modal-content">
+        <div className="modal-header">
+          <h1> Add your own Receipe! </h1>
         </div>
-  
+        <div className="modal-body">
+          <h3> Enter the name of your Recipe! </h3>
+            <div>
+              <input type = "text" placeholder = "Title" id = 'title'
+              value = {this.state.title} onChange = {this.handleChange}
+              required/>
+            </div>
+          <h3> Enter a Description of your Recipe! </h3>
+            <div>
+              <input type = "text" placeholder = "Description" id = 'content'
+              value = {this.state.content} onChange = {this.handleChange}
+              required/>
+            </div>
+          <h3> Insert a picture of the Recipe!  </h3>
+            <div>
+              <input type = "file" id = "image" accept = "image/png, image/jpeg"
+              onChange = {this.handleImageChange}
+              required/>
+            </div>
+        </div>
+        <div className="modal-footer">
+          <Button type = "button" onClick = {() => onSave(this.state.activeItem)}>
+            Submit Now! 
+          </Button>
+        </div>
       </div>
-    )
+    </form>
+  </div>);
+  }
 }
 
 export default Modal
--- frontend/src/pages/recipesPage.js
+++ frontend/src/pages/recipesPage.js
@@ -1,16 +1,73 @@
 import React from 'react';
 import Modal from './modal';
+import {Component} from 'react';
 
-function recipesPage() {
-  return (
-    <div>
-          <h1> Popular Recipes </h1>
+class recipesPage extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      modal: false, 
+      activeItem: {
+        title: "", 
+        description: "", 
+        image: null, 
+      }, 
+    };
+  }
+
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+
+  handleSubmit = (item) => {
+    this.toggle();
+    alert("save" + JSON.stringify(item));
+  };
+
+  handleDelete = (item) => {
+    alert("delete" + JSON.stringify(item));
+  };
+
+  createItem = () => {
+    const item = { title: "", description: "", completed: false };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+
+  editItem = (item) => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+
+  render() {
+    return (
+      <div>
+            <h1> Popular Recipes </h1>
+            <div class="row">
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"> Taco Salad </h5>
+                  <p class="card-text"> Winner on April 4th! </p>
+                  <a href="#" class="btn btn-primary"> Recipe </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"> Chicken Fried Rice </h5>
+                  <p class="card-text"> Winner on March 28th! </p>
+                  <a href="#" class="btn btn-primary"> Recipe </a>
+                </div>
+              </div>
+            </div>
+          </div>
           <div class="row">
           <div class="col-sm-6">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title"> Taco Salad </h5>
-                <p class="card-text"> Winner on April 4th! </p>
+                <h5 class="card-title"> Veggie Quesadillas </h5>
+                <p class="card-text">Winner on February 22nd! </p>
                 <a href="#" class="btn btn-primary"> Recipe </a>
               </div>
             </div>
@@ -18,41 +75,41 @@ function recipesPage() {
           <div class="col-sm-6">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title"> Chicken Fried Rice </h5>
-                <p class="card-text"> Winner on March 28th! </p>
+                <h5 class="card-title"> Fajitas</h5>
+
+                <p class="card-text"> Winner on January 1st! </p>
                 <a href="#" class="btn btn-primary"> Recipe </a>
               </div>
             </div>
           </div>
         </div>
+        <h1> Past Recipes </h1>
         <div class="row">
-        <div class="col-sm-6">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title"> Veggie Quesadillas </h5>
-              <p class="card-text">Winner on February 22nd! </p>
-              <a href="#" class="btn btn-primary"> Recipe </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title"> Fajitas</h5>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"> Brocoli Pesto Pasta </h5>
 
-              <p class="card-text"> Winner on January 1st! </p>
-              <a href="#" class="btn btn-primary"> Recipe </a>
+                  <a href="#" class="btn btn-primary"> Recipe </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"> Instant Pot Mac and Cheese </h5>
+                  
+                  <a href="#" class="btn btn-primary"> Recipe </a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <h1> Past Recipes </h1>
-      <div class="row">
+          <div class="row">
           <div class="col-sm-6">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title"> Brocoli Pesto Pasta </h5>
-
+                <h5 class="card-title"> Cauliflower Gnocci </h5>
+                
                 <a href="#" class="btn btn-primary"> Recipe </a>
               </div>
             </div>
@@ -60,7 +117,7 @@ function recipesPage() {
           <div class="col-sm-6">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title"> Instant Pot Mac and Cheese </h5>
+                <h5 class="card-title"> Roasted Red Pepper Pasta </h5>
                 
                 <a href="#" class="btn btn-primary"> Recipe </a>
               </div>
@@ -68,30 +125,28 @@ function recipesPage() {
           </div>
         </div>
         <div class="row">
-        <div class="col-sm-6">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title"> Cauliflower Gnocci </h5>
-              
-              <a href="#" class="btn btn-primary"> Recipe </a>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Spicy Garlic Ramen Noodles</h5>
+                  <a href="#" class="btn btn-primary"> Recipe </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"> Falafel Pita Pocket </h5>
+                  <a href="#" class="btn btn-primary"> Recipe </a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title"> Roasted Red Pepper Pasta </h5>
-              
-              <a href="#" class="btn btn-primary"> Recipe </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
+          <div class="row">
           <div class="col-sm-6">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title">Spicy Garlic Ramen Noodles</h5>
+                <h5 class="card-title"> Shrimp Alfredo Scampi </h5>
                 <a href="#" class="btn btn-primary"> Recipe </a>
               </div>
             </div>
@@ -99,38 +154,27 @@ function recipesPage() {
           <div class="col-sm-6">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title"> Falafel Pita Pocket </h5>
+                <h5 class="card-title"> Roasted Brussel Sprouts</h5>
                 <a href="#" class="btn btn-primary"> Recipe </a>
               </div>
             </div>
           </div>
         </div>
-        <div class="row">
-        <div class="col-sm-6">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title"> Shrimp Alfredo Scampi </h5>
-              <a href="#" class="btn btn-primary"> Recipe </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title"> Roasted Brussel Sprouts</h5>
-              <a href="#" class="btn btn-primary"> Recipe </a>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      <div className = "Submit Section">
-        <button> Add a Recipe </button>
-        <Modal />
-      </div>
+        <div className = "Submit Section">
+          <button onClick={this.createItem}> Add a Recipe </button>
+        </div>
+       
+          <Modal
+            activeItem = {this.state.activeItem}
+            toggle = {this.toggle}
+            onSave = {this.handleSubmit}
+          />
 
-    </div>
-  );
+      </div>
+    );
+  };
+
 }
 
 export default recipesPage;
